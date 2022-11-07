0bba33b13d4daf20afbde3828f8cfa8ed0495ad0
Apple <riyabulia12@gmail.com>
2022-11-07 19:03:25 +0530

    Add age param to constructors

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require './person'
 # extends person class
 class Student < Person
   # extends parent's constructor by adding @classroom and a parameter for it
-  def initialize(classroom, name = 'Unknown', parent_permission: true)
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require './person'
 # extends person class
 class Teacher < Person
   # extends person constructor and @specialization
-  def initialize(specialization, name = 'Unknown', parent_permission: true)
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @specialization = specialization
   end
