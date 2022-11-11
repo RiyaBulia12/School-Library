469e087292c544665bd6c8e4f97bbe4d445a5bda
Apple <riyabulia12@gmail.com>
2022-11-11 14:09:37 +0530

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,7 +1,3 @@
-# Present the user with a list of options to perform.
-# Lets users choose an option.
-# If needed, ask for parameters for the option.
-# Have a way to quit the app.
 require './app'
 
 def main
