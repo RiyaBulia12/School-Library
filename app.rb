5d7b05861513ac7903c99f361f928741d2d4d2b8
Apple <riyabulia12@gmail.com>
2022-11-11 12:31:40 +0530

    Update app.rb

--- app.rb
+++ app.rb
@@ -31,12 +31,10 @@ def list_rental
   person_id = gets.chomp.to_i
 
   @rentals.each do |rental|
-    p rental
-    p rental.book
-    # if rental.person.id == person_id
-    #   puts "Date: #{rental.date} \nBook: #{rental.book.title} \nAuthor: #{rental.book.author}"
-    #   puts "--------------------------------------------------------"
-    # end
+    if rental.person.id == person_id
+      puts "Date: #{rental.date} \nBook: #{rental.book.title} \nAuthor: #{rental.book.author}"
+      puts "--------------------------------------------------------"
+    end
   end
 end
 
@@ -104,15 +102,13 @@ end
 def create_rental
   puts 'Select a book from the following list by number'
   list_books
+  print 'Book Id: '
+  book_id = gets.chomp.to_i
 
   puts 'Select a person from the following list by number (Not ID): '
   list_people
-
   print 'Person Id: '
   person_id = gets.chomp.to_i
-
-  print 'Book Id: '
-  book_id = gets.chomp.to_i
 
   print 'Date: '
   date = gets.chomp
@@ -162,122 +158,3 @@ class App
     end
   end
 end
-
-# puts "Welcome to School Library App""
-# puts "Please choose an option by entering a number:"
-# puts "1. List all books"
-# puts "2. List all people"
-# puts "3. Create a person"
-# puts "4. Create a book"
-# puts "5. Create a rental"
-# puts "6. List all rentals for a given person id"
-# puts "7. Exit"
-#
-# def list_books
-#   puts "List of books"
-#   @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
-# end
-#
-# def list_people
-#   puts "List of people"
-#   @people.each { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
-# end
-#
-# def create_person
-#   puts "Do you want to create a student (1) or a teacher (2)? [Input the number]:"
-#   option = gets.chomp
-#
-#   case option
-#   when "1"
-#     create_student
-#   when "2"
-#     create_teacher
-#   else
-#     puts "That is not a valid input"
-#     return
-#   end
-# end
-#
-# def create_student
-#   puts "Age:"
-#   age = gets.chomp
-#
-#   puts "Name:"
-#   name = gets.chomp
-#
-#   puts "Has parent permission? [Y/N]"
-#   parent_permission = gets.chomp
-#   parent_permission = parent_permission.downcase == "y"
-#
-#   student = Student.new(age, name, parent_permission: parent_permission)
-#   @people << student
-#   puts "Person created successfully"
-# end
-#
-# def create_teacher
-#   puts "Age:"
-#   age = gets.chomp
-#
-#   puts "Name:"
-#   name = gets.chomp
-#
-#   puts "Specialization:"
-#   specialization = gets.chomp
-#
-#   teacher = Teacher.new(age, specialization, name)
-#   @people << teacher
-#   puts "Person created successfully"
-# end
-#
-# def create_book
-#   puts "Title:"
-#   title = gets.chomp
-#
-#   puts "Author:"
-#   author = gets.chomp
-#
-#   book = Book.new(title, author)
-#   @books << book
-#   puts "Book created successfully"
-# end
-#
-# def create_rental
-#   puts "ID of person:"
-#   person_id = gets.chomp
-#
-#   puts "ID of book:"
-#   book_id = gets.chomp
-#
-#   puts "Date:"
-#   date = gets.chomp
-#
-#   rental = Rental.new(date, book_id, person_id)
-#   @rentals << rental
-#   puts "Rental created successfully"
-# end
-#
-# def list_rentals_for_person_id
-#   puts "ID of person:"
-#   id = gets.chomp
-#
-#   rentals = @rentals.filter { |rental| rental.person_id == id }
-#   puts "Rentals:"
-#   rentals.each do |rental|
-#     puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
-#   end
-# end
-#
-# def main
-#
-# end
-#
-# def app
-#   @books = []
-#   @people = []
-#   @rentals = []
-#
-#   main while true
-# end
-#
-# app
-#
