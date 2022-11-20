16ffba414907191e7f02878f4cf912945f6d6a23
Apple <riyabulia12@gmail.com>
2022-11-20 20:12:07 +0530

    Fixed parser error

--- student.rb
+++ student.rb
@@ -6,7 +6,7 @@ class Student < Person
 
   # extends parent's constructor by adding @classroom and a parameter for it
   def initialize(age, name: 'Unknown', parent_permission: true)
-    super(age, name:, parent_permission:)
+    super(age, name: name, parent_permission: parent_permission)
     @classroom = nil
   end
 
